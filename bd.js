1f20a007bee86238eca0f09d59e327c7458dd336
Ian Alves <132668846+Alvesds-Ian@users.noreply.github.com>
2023-11-29 00:09:20 +0000

    membro - pronto

--- bd.js
+++ bd.js
@@ -29,4 +29,19 @@ async function insertMembro(data) {
   await client.query(query, membro);
 }
 
-export { selectMembros, selectMembro, insertMembro };
+async function deleteMembro(email) {
+  const client = await connect();
+  const query = "DELETE FROM membro WHERE email = $1";
+  await client.query(query, [email]);
+}
+
+async function updateMembro(data, emailToUpdate) {
+  const client = await connect();
+  const query =
+    "UPDATE membro SET nome = $1, email = $2, senha = $3, cargo = $4, funcao = $5 WHERE email = $6";
+  const membro = [data.nome, data.email, data.senha, data.cargo, data.funcao, emailToUpdate];
+  await client.query(query, membro);
+}
+
+
+export { selectMembros, selectMembro, insertMembro, deleteMembro, updateMembro };
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-//index.js
 import dotenv from "dotenv";
 import express from "express";
-import { selectMembro, selectMembros, insertMembro } from "./bd.js";
+import { selectMembro, selectMembros, insertMembro, deleteMembro, updateMembro } from "./bd.js";
 
 dotenv.config();
 
 const app = express(); 
 const port = 3000; 
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Rota / solicitada");
@@ -17,12 +18,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/membros", async (req, res) => {
+  console.log("Rota GET/membros solicitada");
   try {
     const membros = await selectMembros();
     res.json(membros);
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
+  });
 
 app.get("/membro/:email", async (req, res) => {
     console.log("Rota GET /membro solicitada");
@@ -45,10 +48,36 @@ app.post("/membro", async (req, res) => {
   }
 });
 
-  console.log("Rota GET/membros solicitada");
+app.delete("/membro/:email", async (req, res) => {
+  console.log("Rota DELETE /membro solicitada");
+  try {
+    const membro = await selectMembro(req.params.id);
+    if (membro.length > 0) {
+      await deleteMembro(req.params.email);
+      res.status(200).json({ message: "Membro excluido com sucesso!!" });
+    } else res.status(404).json({ message: "Membro não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
 
-app.use(express.json());
+app.put("/membro", async (req, res) => {
+  console.log("Rota PUT /membro solicitada");
+  const newEmail = req.body.newEmail; // Adicione essa linha para obter o novo email do corpo da requisição
+  try {
+    const membro = await selectMembro(req.body.email);
+    if (membro.length > 0) {
+      await updateMembro(req.body, newEmail); // Atualize para passar o novo email como segundo parâmetro
+      res.status(200).json({ message: "Membro atualizado com sucesso!" });
+    } else {
+      res.status(404).json({ message: "Membro não encontrado!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
 
 app.listen(port, () => {
   // Um socket para "escutar" as requisições
